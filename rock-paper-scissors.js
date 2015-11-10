68afb39ad78cda61fc134fa45725f3de53e3f1a1
jbvanblake <josh@vanblake.com>
2015-11-09 21:34:25 -0800

    adding more functionality and styles.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,5 +1,22 @@
 Matches = new Mongo.Collection("matches");
-player = 0
+ROCK_PAPER_SCISSORS_KEY={
+    "Rock":{
+        "Rock":0,
+        "Paper":-1,
+        "Scissors":1
+    },
+    "Paper":{
+        "Rock":1,
+        "Paper":0,
+        "Scissors":-1
+    },
+    "Scissors":{
+        "Rock":-1,
+        "Paper":1,
+        "Scissors":0
+    }
+}
+player = "Player0"
 currentMatch = undefined
 
 
@@ -10,10 +27,22 @@ if (Meteor.isClient) {
     // This code only runs on the client
     Template.body.helpers({
         history: function () {
-            return Matches.find ({$and:[{player1: { $exists: true } }, {player2: { $exists: true } }]});
+            return Matches.find ({$and:[{player1: { $exists: true } }, {player2: { $exists: true } }]}, {sort: {createdAt: -1}});
+        },
+        mostRecentMatch: function () {
+            return Matches.findOne({$and:[{player1: { $exists: true } }, {player2: { $exists: true } }]}, {sort: {createdAt: -1}});
         },
         player: function(){
             return player;
+        },
+        playerPretty: function(){
+            if(player == "player1"){
+                return "Player 1";
+            }
+            else if(player == "player2"){
+                return "Player 2";
+            }
+            return "No one"
         },
         currentMatch: function () {
             currentMatch = Matches.findOne ({$or:[{player1: { $exists: false } },{player2: { $exists: false } }]})
@@ -38,6 +67,28 @@ if (Meteor.isClient) {
         },
         "click .new-game": function (event) {
             Matches.insert({createdAt:new Date()});
+        },
+        "click .toggle-history": function (event) {
+            if($(".history-container:visible").length >0){
+                $(".history-container").hide();
+            }
+            else{
+                $(".history-container").show();
+            }
+
+        },
+        "click .clear-history": function (event) {
+         if($(".confirm-container:visible").length === 0){
+             $(".confirm-container").show();
+         }
+
+        },
+        "click .confirm-clear": function (event) {
+            Meteor.call('removeAllMatches');
+            $(".confirm-container").hide();
+        },
+        "click .cancel-clear": function (event) {
+            $(".confirm-container").hide();
         }
     });
 
@@ -45,7 +96,7 @@ if (Meteor.isClient) {
         return date.toUTCString();
     });
     Template.registerHelper('myTurn', function(player, match) {
-        if(!match.player1 && match.player2){
+        if(!match.player1 && !match.player2){
             return true;
         }
         if(!match.player1 && player === "player1"){
@@ -56,15 +107,80 @@ if (Meteor.isClient) {
         }
         return false;
     });
+    Template.registerHelper('myChoice', function(match) {
+      return myChoice(match);
+
+    });
+    Template.registerHelper('hisChoice', function(match) {
+        return hisChoice(match);
+
+    });
+    Template.registerHelper('resultWin', function(match) {
+        return rockPaperScissors(myChoice(match), hisChoice(match)) === 1;
+
+    });
+    Template.registerHelper('resultLoss', function(match) {
+        return rockPaperScissors(myChoice(match), hisChoice(match)) === -1;
+
+    });
+    Template.registerHelper('prettyResult', function(match) {
+        var result = rockPaperScissors(myChoice(match), hisChoice(match));
+        if(result === 0){
+            return "You Tied!";
+        }
+        if(result === 1){
+            return "You Won!";
+        }
+        if(result === -1){
+            return "You Lost!";
+        }
+        return "??";
+    });
+}
+var myChoice = function(match){
+    if(player == "player1"){
+        return match.player1
+    }
+    else if(player == "player2"){
+        return match.player2
+    }
+    return undefined;
+}
+var hisChoice = function(match){
+    if(player == "player1"){
+        return match.player2
+    }
+    else if(player == "player2"){
+        return match.player1
+    }
+    return undefined;
+
+}
+var rockPaperScissors = function(action1, action2){
+    if(ROCK_PAPER_SCISSORS_KEY[action1] !==undefined){
+        if(ROCK_PAPER_SCISSORS_KEY[action1][action2] !==undefined){
+            return ROCK_PAPER_SCISSORS_KEY[action1][action2];
+        }
+    }
+    return undefined;
 }
 
-FlowRouter.route('/player1', {
-    action: function() {
-        player="player1"
+
+FlowRouter.route('/:playerName', {
+    action: function(params) {
+        player=params.playerName
     }
 });
-FlowRouter.route('/player2', {
-    action: function() {
-        player="player2"
-    }
-});
+
+if (Meteor.isServer) {
+    Meteor.startup(function() {
+        return Meteor.methods({
+            removeAllMatches: function() {
+                return Matches.remove({});
+            }
+
+        });
+
+    });
+
+}
