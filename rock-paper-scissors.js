16a5e2313e5c3bbd4c893c7d1d4bffc44dcaf0ab
jbvanblake <josh@vanblake.com>
2015-11-09 16:02:35 -0800

    starting functionality.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,23 +1,67 @@
+Matches = new Mongo.Collection("matches");
+player = 0
+currentMatch = undefined
+
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+    currentMatch = Matches.findOne ({$or:[{player1: { $exists: false } },{player2: { $exists: false } }]});
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+    // This code only runs on the client
+    Template.body.helpers({
+        history: function () {
+            return Matches.find ({$and:[{player1: { $exists: true } }, {player2: { $exists: true } }]});
+        },
+        player: function(){
+            return player;
+        },
+        currentMatch: function () {
+            currentMatch = Matches.findOne ({$or:[{player1: { $exists: false } },{player2: { $exists: false } }]})
+            return currentMatch;
+        }
+    });
+    Template.body.events({
+        "click .action": function (event) {
+
+            var action = event.target.value;
+
+            if(player === "player1"){
+                Matches.update(currentMatch._id, {
+                    $set: {player1: action}
+                });
+            }
+            else{
+                Matches.update(currentMatch._id, {
+                    $set: {player2: action}
+                });
+            }
+        }
+    });
+
+    Template.registerHelper('formatDate', function(date) {
+        return date.toUTCString();
+    });
+    Template.registerHelper('myTurn', function(player, match) {
+        if(!match.player1 && match.player2){
+            return true;
+        }
+        if(!match.player1 && player === "player1"){
+            return true;
+        }
+        if(!match.player2 && player === "player2"){
+            return true;
+        }
+        return false;
+    });
 }
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
+FlowRouter.route('/player1', {
+    action: function() {
+        player="player1"
+    }
+});
+FlowRouter.route('/player2', {
+    action: function() {
+        player="player2"
+    }
+});
